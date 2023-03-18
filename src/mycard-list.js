202080ff246761a9d04d08043921ba661667e6f7
AaronLobitana <faaron.lobitana@gmail.com>
2023-03-18 14:29:16 -0400

    mini-resume
    attempting /api capabilities

---
+++ src/mycard-list.js
@@ -0,0 +1,102 @@
+import { LitElement, html, css } from 'lit';
+import "./mini-resume.js";
+
+
+export class MyCardList extends LitElement{
+
+    static get tag() {
+        return 'mycard-list';
+    }
+
+    static get properties() {
+        return {
+            cards: { type: Array },
+            name: { type: String },
+        }
+
+    }
+
+    constructor() {
+        super();
+        this.cards = [
+            [
+                {
+                    "imageURL": "https://i.imgur.com/g4oneXM.jpeg",
+                    "name": "Aaron Lobitana",
+                    "extrainfolabel": "Extra Information",
+                    "shadowEnable": "false",
+                    "newColor": "null",
+                    "memeTop": "me: doesn't have a professional headshot",
+                    "memeBottom": "also me:",
+                    "opened": "false"
+            
+                },
+                {
+                    "imageURL": "https://i.imgur.com/g4oneXM.jpeg",
+                    "name": "Aaron Lobitana",
+                    "extrainfolabel": "Extra Information",
+                    "shadowEnable": "false",
+                    "newColor": "null",
+                    "memeTop": "me: doesn't have a professional headshot",
+                    "memeBottom": "also me:",
+                    "opened": "false"
+            
+                },
+                {
+                    "imageURL": "https://i.imgur.com/g4oneXM.jpeg",
+                    "name": "Aaron Lobitana",
+                    "extrainfolabel": "Extra Information",
+                    "shadowEnable": "false",
+                    "newColor": "null",
+                    "memeTop": "me: doesn't have a professional headshot",
+                    "memeBottom": "also me:",
+                    "opened": "false"
+            
+                }
+            ]
+            
+            
+        ];
+        this.name = 'Aaron Lobitana';
+    }
+
+
+    static get styles(){
+        return css`
+        
+        :host{
+            display: block;
+        }
+        .box{
+            border: 2px solid black;
+            display: flex;
+        }
+        .item{
+            display: inline-flex;
+        }
+
+
+    `;
+    }
+
+    render() {
+        return html`
+        <h2>${this.name}</h2>
+        <div class="box">
+
+            ${this.cards.map(card => html`
+            <div class="item">
+                <mini-resume imageURL="${card.imageURL}" name="${card.name}" extrainfolabel="${card.extrainfolabel}" ?shadowEnable="${card.shadowEnable}" newColor="${card.newColor}" memeTop="${card.memeTop}" memeBottom="${card.memeBottom}" ?opened="${card.opened}"></mini-resume>
+            </div>
+            `)}
+
+        </div>
+
+
+
+        `;
+    }
+    
+}
+
+customElements.define(MyCardList.tag, MyCardList);
